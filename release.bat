c4cfa0c3a9c2bc8ab700f51b102d7b4c5518a45a
Sergey Nechaev <serg.nechaev@gmail.com>
2018-11-03 12:27:49 +1100

    Update release.bat

--- release.bat
+++ release.bat
@@ -3,4 +3,4 @@ set JAVA_HOME=C:\tools\Java\jdk1.8.0_191
 set PATH=%PATH%;C:\tools\Java\jdk8.0_191\bin;
 
 set MAVEN_OPTS=-Xmx2G
-C:\tools\apache-maven-3.5.4\bin\mvn.cmd clean release:prepare release:perform
+C:\tools\apache-maven-3.5.4\bin\mvn.cmd clean release:clean release:prepare release:perform
